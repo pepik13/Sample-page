a52d518b4c07a7c21efa269927a7b9883bdb1645
Damian Zawidzki <pepik130690@gmail.com>
2025-08-13 18:38:18 +0100

    about us

---
+++ about.php
@@ -0,0 +1,6 @@
+<?php include "header.php"; ?>
+<h1>Contact Us</h1>
+<p>Email: support@electro.demo</p>
+<p>Phone: +44 0000 000000</p>
+<img src="images/team.jpg" alt="Our Team" class="aboutimg"/>
+<?php include "footer.php"; ?>
